8e97d232c8d529de2bd72acd00c8e8f5c680e6c5
Luci <98768417+Diacod-I@users.noreply.github.com>
2023-12-11 00:10:22 +0530

    Random Number Generator

--- Header Files/Layer.h
+++ Header Files/Layer.h
@@ -7,6 +7,12 @@
 #include "Optimizer.h"
 
 class Layer{
+    protected:
+        typedef Eigen::Matrix<Scalar, Eigen::Dynamic, Eigen::Dynamic> Matrix;
+        typedef Eigen::Matrix<Scalar, Eigen::Dynamic, 1> Vector;
+        const int m_in_size;
+        const int m_out_size;
+    
     public:
         Layer(const int in_size, const int out_size);
             m_in_size(in_size),
@@ -18,12 +24,17 @@ class Layer{
         int out_size() const { return m_out_size; }
 
         virtual void init(const Scalar& mu, const Scalar &sigma, RandomNum& rng) = 0;
-    protected:
-        typedef Eigen::Matrix<Scalar, Eigen::Dynamic, Eigen::Dynamic> Matrix;
-        typedef Eigen::Matrix<Scalar, Eigen::Dynamic, 1> Vector;
-        const int m_in_size;
-        const int m_out_size;
-    
+        virtual void forward(const Matrix& prev_layer_output) = 0;
+
+        virtual const Matrix& output() const = 0;
+
+        virtual void backprop(const Matrix& prev_layer_output, const Matrix& next_layer_data) = 0;
+        virtual const Matrix& backprop() const=0;
+
+        virtual std::vector<Scalar> get_parameter() const = 0;
+        virtual void set_parameter(const std::vector<Scalar>& param) {}
+        virtual std::vector<Scalar> get_derivatives() const = 0; 
+
     private:
 
 };
