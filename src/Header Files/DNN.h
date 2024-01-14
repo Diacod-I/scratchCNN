bdf0d55f2d24cbf61d8179f755d24b812c218dd1
Luci <98768417+Diacod-I@users.noreply.github.com>
2024-01-15 02:38:09 +0530

    Added src and lib

---
+++ src/Header Files/DNN.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <Eigen/Core>
+#include "RandomNum.h"
+#include "Config.h"
+#include "Layer.h"
+#include "Layers/FClayer.h"
+#include "Layers/Conv2dlayer.h"
+#include "Layers/Poolinglayer.h"
---
+++ src/Header Files/Layers/FClayer.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <Eigen/Core>
+#include <vector> //Library for creating sequence container objects known as vectors
+#include <stdexcept> //Useful for setting arithmetic exceptions
+#include "../Config.h"
+#include "../Layer.h"
+
+class FClayer: public Layer {
+
+};
